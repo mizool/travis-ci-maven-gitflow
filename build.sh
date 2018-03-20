a1f92d1eacf00564c48b2fd3f87239ab39cdaa1c
Sven Haberer <schroenser@gmail.com>
2018-03-20 09:48:31 +0100

    Fixed github URLs.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-wget -q -O $HOME/.m2/settings.xml https://github.com/mizool/travis-ci-maven-gitflow/blob/feature/create-initial-files-and-documentation/settings.xml
-wget -q -O $HOME/.m2/toolchains.xml https://github.com/mizool/travis-ci-maven-gitflow/blob/feature/create-initial-files-and-documentation/toolchains.xml
+wget -q -O $HOME/.m2/settings.xml https://raw.githubusercontent.com/mizool/travis-ci-maven-gitflow/feature/create-initial-files-and-documentation/settings.xml
+wget -q -O $HOME/.m2/toolchains.xml https://raw.githubusercontent.com/mizool/travis-ci-maven-gitflow/feature/create-initial-files-and-documentation/toolchains.xml
 
 if [[ ( $TRAVIS_BRANCH = 'master' || $TRAVIS_BRANCH = 'develop' ) && $TRAVIS_PULL_REQUEST = 'false' ]]; then
     openssl aes-256-cbc -in codesigning.asc.enc -out codesigning.asc -d -pass pass:$CODESIGNING_AES_PASSWORD
