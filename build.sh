581bc6382e643b16eed043eedffe4b52075340ad
Sven Haberer <schroenser@gmail.com>
2018-03-21 08:17:51 +0100

    Release and hotfix branch snapshots now also get deployed.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 wget -q -O $HOME/.m2/settings.xml https://raw.githubusercontent.com/mizool/travis-ci-maven-gitflow/master/settings.xml
 wget -q -O $HOME/.m2/toolchains.xml https://raw.githubusercontent.com/mizool/travis-ci-maven-gitflow/master/toolchains.xml
 
-if [[ ( $TRAVIS_BRANCH = 'master' || $TRAVIS_BRANCH = 'develop' ) && $TRAVIS_PULL_REQUEST = 'false' ]]; then
+if [[ ( $TRAVIS_BRANCH = master || $TRAVIS_BRANCH = develop || $TRAVIS_BRANCH = release/* || $TRAVIS_BRANCH = hotfix/*) && $TRAVIS_PULL_REQUEST = false ]]; then
     openssl aes-256-cbc -in codesigning.asc.enc -out codesigning.asc -d -pass pass:$CODESIGNING_AES_PASSWORD
     gpg --batch --quiet --fast-import codesigning.asc
     mvn deploy -DperformRelease=true -P sign
