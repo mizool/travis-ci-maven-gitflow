eb2a975232b4f78e45a58eecbe62d98bc4a033de
Sven Haberer <schroenser@gmail.com>
2018-05-17 12:44:55 +0200

    Added the OSSRH snapshot repository to the settings.xml and added the maven parameter to always update snapshots.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ wget -q -O $HOME/.m2/toolchains.xml https://raw.githubusercontent.com/mizool/tra
 if [[ ( $TRAVIS_BRANCH = master || $TRAVIS_BRANCH = develop || $TRAVIS_BRANCH = release/* || $TRAVIS_BRANCH = hotfix/*) && $TRAVIS_PULL_REQUEST = false ]]; then
     openssl aes-256-cbc -in codesigning.asc.enc -out codesigning.asc -d -pass pass:$CODESIGNING_AES_PASSWORD
     gpg --batch --quiet --fast-import codesigning.asc
-    mvn deploy -DperformRelease=true -P sign
+    mvn -U deploy -DperformRelease=true -P sign
 else
-    mvn verify -DperformRelease=true
+    mvn -U verify -DperformRelease=true
 fi
